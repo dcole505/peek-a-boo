408ef0494be8e9c9a9b578103bfd155bbc68ab46
David Cole <davidtcole@yahoo.com>
2020-06-02 18:15:46 -0700

    add new paragraph elements, css styles, and jquery to add the fadeToggle functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
     $("#walrus-hidden").toggle();
   });
 });
+
+$(document).ready(function() {
+  $(".fadeable").click(function() {
+    $("#walrus-fade-in").fadeToggle();
+    $("#walrus-fade-out").fadeToggle();
+  });
+});
